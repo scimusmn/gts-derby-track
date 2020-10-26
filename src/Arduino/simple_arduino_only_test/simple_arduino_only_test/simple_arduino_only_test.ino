980be9491ec32b63f1dbefc79f9178b7c2cc990c
Joe Meyer <joseph.c.meyer@gmail.com>
2020-10-26 12:39:16 -0500

    auto formating

--- src/Arduino/simple_arduino_only_test/simple_arduino_only_test/simple_arduino_only_test.ino
+++ src/Arduino/simple_arduino_only_test/simple_arduino_only_test/simple_arduino_only_test.ino
@@ -1,82 +1,91 @@
 int solenoid_pin = 6;
 int led_pin = 13;
-bool racing[] = {false,false,false};
-int start_pins[] = {10,11,12};
-int finish_pins[] = {2,3,4};
+bool racing[] = {false, false, false};
+int start_pins[] = {10, 11, 12};
+int finish_pins[] = {2, 3, 4};
 int start_btn = 8;
 int start_btn_led = 9;
 
 unsigned long start_time = 0;
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   // put your setup code here, to run once:
-  pinMode(solenoid_pin,OUTPUT);
-  digitalWrite(solenoid_pin,LOW);
-  pinMode(led_pin,OUTPUT);
-  digitalWrite(led_pin,LOW);
+  pinMode(solenoid_pin, OUTPUT);
+  digitalWrite(solenoid_pin, LOW);
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
   pinMode(start_btn, INPUT);
   pinMode(start_btn_led, OUTPUT);
 
-  for (int i=0; i<3; i++){
-    pinMode(start_pins[i],INPUT);
-    pinMode(finish_pins[i],INPUT);
+  for (int i = 0; i < 3; i++)
+  {
+    pinMode(start_pins[i], INPUT);
+    pinMode(finish_pins[i], INPUT);
   }
-
 }
 
-void loop() {
-  if (!digitalRead(start_btn)){
-    digitalWrite(solenoid_pin,HIGH);
+void loop()
+{
+  if (!digitalRead(start_btn))
+  {
+    digitalWrite(solenoid_pin, HIGH);
     digitalWrite(start_btn_led, LOW);
     delay(500);
     start_time = millis();
-    digitalWrite(solenoid_pin,LOW);
+    digitalWrite(solenoid_pin, LOW);
     digitalWrite(start_btn_led, HIGH);
   }
 
-
-  if (!digitalRead(start_pins[0]) && (racing[2] == false)){
+  if (!digitalRead(start_pins[0]) && (racing[2] == false))
+  {
     Serial.println("track 3 start");
     racing[2] = true;
   }
-  
-  if (!digitalRead(start_pins[1]) && (racing[1] == false)){
+
+  if (!digitalRead(start_pins[1]) && (racing[1] == false))
+  {
     Serial.println("track 2 start");
-    racing[1]=true;
-  }
-  
-  if (!digitalRead(start_pins[2]) && (racing[0] == false)){
-    Serial.println("track 1 start");
-    racing[0]=true;
+    racing[1] = true;
   }
 
-  if (!digitalRead(finish_pins[0])){
-    if (racing[0] == true){
+  if (!digitalRead(start_pins[2]) && (racing[0] == false))
+  {
+    Serial.println("track 1 start");
+    racing[0] = true;
+  }
+
+  if (!digitalRead(finish_pins[0]))
+  {
+    if (racing[0] == true)
+    {
       Serial.print("Track 1 time:");
       unsigned long finish_time = millis() - start_time;
       Serial.println(finish_time);
       racing[0] = false;
     }
   }
-  
-  if (!digitalRead(finish_pins[1])) {
-    if(racing[1] == true){
+
+  if (!digitalRead(finish_pins[1]))
+  {
+    if (racing[1] == true)
+    {
       Serial.print("Track 2 time:");
       unsigned long finish_time = millis() - start_time;
       Serial.println(finish_time);
       racing[1] = false;
     }
   }
-   
-  if (!digitalRead(finish_pins[2])){
-    if (racing[2] == true){
+
+  if (!digitalRead(finish_pins[2]))
+  {
+    if (racing[2] == true)
+    {
       Serial.print("Track 3 time:");
       unsigned long finish_time = millis() - start_time;
-      Serial.println(finish_time);    
+      Serial.println(finish_time);
       racing[2] = false;
     }
   }
-  
 }
